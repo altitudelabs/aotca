6f86bfa1fa9094976bee05609dd7da89ab6eaaa0
tszyanalau <amy@altitudelabs.com>
2017-08-10 18:27:43 +0800

    - tidy up code - adjust wordings

--- wp-content/plugins/aotca-builder/templates/template-member-detail.php
+++ wp-content/plugins/aotca-builder/templates/template-member-detail.php
@@ -26,41 +26,40 @@ $container_class = implode( ' ', apply_filters( 'themify_builder_module_classes'
 	'module', 'module-' . $mod_name, $module_ID
 	), $mod_name, $module_ID, $fields_args )
 );
-?>
+
+function render_member_info ($key, $value, $url=false) { ?>
+	<?php if (!empty($value)):?>
+		<div class="field">
+		<span class="key"><?php echo $key?></span>
+		<?php if ($url == false):?>
+		<span class="value"><?php echo $value?></span>
+	<?php else:?>
+		<a class="value" href="<?php echo $value?>"><?php echo $value?></a>
+	<?php endif;?>
+	  </div>
+  <?php endif;?>
+<?php } ?>
 
 <div id="<?php echo $module_ID; ?>" class="<?php echo esc_attr( $container_class ); ?>">
   <div id="aotca-content-container">
     <section class="info">
-      <div class="logo-container">
-        <img src="<?php echo $member_detail_logo?>" />
+      <div class="logo-container"><div class="logo-wrapper">
+        <img src="<?php echo $member_detail_logo?>" /> </div>
       </div>
       <div class="text-info">
-        <h1 class="title"><?php echo $member_detail_title?></h1>
-        <div class="field">
-          <span class="key">Address</span>
-          <span class="value"><?php echo $member_detail_address?></span>
-        </div>
-        <div class="field">
-          <span class="key">Phone</span>
-          <span class="value"><?php echo $member_detail_phone?></span>
-        </div>
-        <div class="field">
-          <span class="key">Fax</span>
-          <span class="value"><?php echo $member_detail_fax?></span>
-        </div>
-        <div class="field">
-          <span class="key">Email</span>
-          <span class="value"><?php echo $member_detail_email?></span>
-        </div>
-        <div class="field">
-          <span class="key">Website</span>
-          <a class="value" href="<?php echo $member_detail_website?>"><?php echo $member_detail_website?></a>
-        </div>
-      </div>
+        <h2 class="title"><?php echo $member_detail_title?></h2>
+		<?php render_member_info("Address", $member_detail_address)?>
+		<?php render_member_info("Phone", $member_detail_phone)?>
+		<?php render_member_info("Fax", $member_detail_fax)?>
+		<?php render_member_info("Email", $member_detail_email)?>
+		<?php render_member_info("Website", $member_detail_website, true)?>
+	      </div>
     </section>
+	<?php if (!empty($member_detail_overview)):?>
     <section class="overview">
-      <h2>Overview</h2>
+      <h3>Organisation Overview</h3>
       <span><?php echo $member_detail_overview?></span>
     </section>
+	<?php endif;?>
   </div>
 </div>
